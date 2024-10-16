03d7c252e2f3b2e18edd611e889fabcbaefcb934
UrielYon <chivasuriel@live.com.mx>
2024-10-15 19:49:54 -0600

    Typeof

--- tipos-de-dato.js
+++ tipos-de-dato.js
@@ -1,1 +1,15 @@
 console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof ",");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+
+console.log(typeof (5+5));
+console.log(typeof 'a');
+console.log(typeof (true&&false));
+console.log(typeof (5===5));
+console.log(typeof "Hola mundo");
